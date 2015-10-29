bbd1d88c0f53784cf1a32658201cc1ffb3912bcb
gam0022 <gam0022@gmail.com>
2015-10-30 01:06:06 +0900

    2ユーザを使ったテスト

--- test.rb
+++ test.rb
@@ -13,10 +13,15 @@ def logs(response)
 end
 
 base_url = 'http://130.211.250.13'
-client1 = HTTPClient.new
 
+client1 = HTTPClient.new
+logs client1.get(base_url + '/initialize')
 logs client1.get(base_url + '/')
 logs client1.post(base_url + '/login', {password: 'eladio4996', email: 'eladio4996@isucon.net'})
 logs client1.get(base_url + '/')
 logs client1.get(base_url + '/diary/comment/947')
 logs client1.get(base_url + '/friends')
+
+client2 = HTTPClient.new
+logs client1.post(base_url + '/login', {password: 'armand875', email: 'armand875@isucon.net'})
+logs client1.get(base_url + '/footprints')
